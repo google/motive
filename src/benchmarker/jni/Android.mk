40f5a691491aaea7d7095cc076bb10a0ba4108b5
Stewart Miles <smiles@google.com>
2015-11-12 19:02:32 -0800

    Fixed build of benchmarker on Android.
    * Added separate build target for motive library with benchmarking
      code enabled.  Setting environment variables for other makefiles
      with ndk-build is brittle.
    
    Tested:
    Verified the app now builds for Android.
    
    Bug: 22510457
    Change-Id: I0a2a05e880c86b40f6012f076a489ffa594c4ee8

--- src/benchmarker/jni/Android.mk
+++ src/benchmarker/jni/Android.mk
@@ -18,7 +18,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := motive_benchmarker
 LOCAL_ARM_MODE := arm
-LOCAL_STATIC_LIBRARIES := libmathfu libmotive
+LOCAL_STATIC_LIBRARIES := libmathfu libmotive_benchmark
 
 MOTIVE_RELATIVE_DIR := ../../..
 MOTIVE_DIR := $(LOCAL_PATH)/$(MOTIVE_RELATIVE_DIR)
@@ -34,9 +34,6 @@ LOCAL_C_INCLUDES := \
   $(MOTIVE_DIR)/src \
   $(DEPENDENCIES_FLATBUFFERS_DIR)/include \
   $(DEPENDENCIES_FPLUTIL_DIR)/libfplutil/include
-
-LOCAL_CFLAGS := -DBENCHMARK_MOTIVE
-MOTIVE_ENABLE_BENCHMARKING := 1
 
 # Uncomment this to run both the C++ and assembly versions of functions
 # and compare the results.
